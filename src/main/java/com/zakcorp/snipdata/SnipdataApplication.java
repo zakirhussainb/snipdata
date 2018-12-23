9cf492529cd5a3dd312814bcafcde9694903b58f
zakirkone <zakir@kloudone.com>
2018-12-23 20:51:03 +0530

    Initial Commit

---
+++ src/main/java/com/zakcorp/snipdata/SnipdataApplication.java
@@ -0,0 +1,14 @@
+package com.zakcorp.snipdata;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SnipdataApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SnipdataApplication.class, args);
+	}
+
+}
+
